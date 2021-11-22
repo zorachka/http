76777d8f1b453b110f2305cc75da49533e8a1b32
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-11-22 11:09:35 +0400

    fix: make all methods static in ResponseFactory

--- src/Http/Response/LaminasResponseFactory.php
+++ src/Http/Response/LaminasResponseFactory.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 namespace Zorachka\Framework\Http\Response;
 
+use Psr\Http\Message\ResponseInterface;
 use Laminas\Diactoros\Response\EmptyResponse;
 use Laminas\Diactoros\Response\HtmlResponse;
 use Laminas\Diactoros\Response\JsonResponse;
 use Laminas\Diactoros\Response\RedirectResponse;
-use Psr\Http\Message\ResponseInterface;
 
 final class LaminasResponseFactory implements ResponseFactory
 {
     /**
      * @inheritDoc
      */
-    public function redirect($uri, int $status = 302, array $headers = []): ResponseInterface
+    public static function redirect($uri, int $status = 302, array $headers = []): ResponseInterface
     {
         return new RedirectResponse($uri, $status, $headers);
     }
@@ -23,7 +23,7 @@ final class LaminasResponseFactory implements ResponseFactory
     /**
      * @inheritDoc
      */
-    public function json(
+    public static function json(
         $data,
         int $status = 200,
         array $headers = [],
@@ -35,7 +35,7 @@ final class LaminasResponseFactory implements ResponseFactory
     /**
      * @inheritDoc
      */
-    public function empty(int $status = 204, array $headers = []): ResponseInterface
+    public static function empty(int $status = 204, array $headers = []): ResponseInterface
     {
         return new EmptyResponse($status, $headers);
     }
@@ -43,7 +43,7 @@ final class LaminasResponseFactory implements ResponseFactory
     /**
      * @inheritDoc
      */
-    public function html($html, int $status = 200, array $headers = []): ResponseInterface
+    public static function html($html, int $status = 200, array $headers = []): ResponseInterface
     {
         return new HtmlResponse($html, $status, $headers);
     }
